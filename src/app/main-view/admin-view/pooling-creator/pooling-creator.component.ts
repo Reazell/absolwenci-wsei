fa3e95bfb74827d0437ed811f189a2d8909d0e13
xAmaris <gabi97_97@o2.pl>
2018-09-05 12:05:57 +0200

    save to change controls

--- src/app/main-view/admin-view/pooling-creator/pooling-creator.component.ts
+++ src/app/main-view/admin-view/pooling-creator/pooling-creator.component.ts
@@ -1,4 +1,4 @@
-/* import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormGroup,
   FormBuilder,
@@ -12,7 +12,11 @@ import {
 })
 export class PoolingCreatorComponent implements OnInit {
   invoiceForm: FormGroup;
-  default = 'dropdown-menu';
+  default = 'single-choice';
+  lastSelect = this.default;
+  disabled = true;
+  time1;
+  time2;
   selects: Select[] = [
     {
       control: [
@@ -35,11 +39,11 @@ export class PoolingCreatorComponent implements OnInit {
     {
       control: [
         {
-          value: 'single-selection-grid',
+          value: 'single-grid',
           viewValue: 'Siatka jednokrotnego wyboru'
         },
         {
-          value: 'multiple-selection-grid',
+          value: 'multiple-grid',
           viewValue: 'Siatka wielokrotnego wyboru'
         }
       ]
@@ -111,8 +115,8 @@ export class PoolingCreatorComponent implements OnInit {
       case 'multiple-choice':
         this.addArray(FieldData, select);
         break;
-      case 'single-selection-grid':
-      case 'multiple-selection-grid':
+      case 'single-grid':
+      case 'multiple-grid':
         this.addSelectionGridField(FieldData, select);
         break;
     }
@@ -121,7 +125,7 @@ export class PoolingCreatorComponent implements OnInit {
   // mains
   addInput(FieldData, select) {
     const group = this.fb.group({
-      input: [{ value: '', disabled: true }]
+      input: [{ value: '', disabled: this.disabled }]
     });
     FieldData.push(group);
     this.addField(select, group.controls.input);
@@ -154,8 +158,8 @@ export class PoolingCreatorComponent implements OnInit {
       case 'multiple-choice':
         this.addCheckField(choiceData, 'opcja');
         break;
-      case 'single-selection-grid':
-      case 'multiple-selection-grid':
+      case 'single-grid':
+      case 'multiple-grid':
         this.addCheckField(choiceData, 'kolumna');
         this.addInputOption(choiceData2, 'wiersz');
         break;
@@ -166,7 +170,7 @@ export class PoolingCreatorComponent implements OnInit {
   addLinearScaleField(choiceArr) {
     const group = this.fb.group({
       minValue: 1,
-      maxValue: 6,
+      maxValue: 5,
       minLabel: [''],
       maxLabel: ['']
     });
@@ -182,10 +186,12 @@ export class PoolingCreatorComponent implements OnInit {
   addCheckField(selectArr, name) {
     const length = selectArr.controls.length;
     const group = this.fb.group({
-      value: [{ value: false, disabled: true }],
+      value: [{ value: false, disabled: this.disabled }],
       viewValue: `${name} ${length}`
     });
     selectArr.push(group);
+    // this.time2 = Date.now();
+    // console.log(this.time2 - this.time1);
   }
 
   removeField(index, SingleChoice) {
@@ -203,310 +209,104 @@ export class PoolingCreatorComponent implements OnInit {
     e.source.checked = true;
   }
 
-  changeControl(controls, select) {
-    controls.removeAt(0);
-    this.addGroup(controls, select);
-  }
-
-  changeSingleValue(single, e) {
-    // single.viewValue = e.target.value;
-  }
-}
-
-export class Control {
-  value: any;
-  viewValue: string;
-  constructor(value: any, viewValue: string) {
-    this.value = value;
-    this.viewValue = viewValue;
-  }
-}
-export class Select {
-  control: Control[];
-  constructor(control: Control[]) {
-    this.control = control;
-  }
-}
-
-export class Value {
-  value: number;
-  constructor(value: number) {
-    this.value = value;
-  }
-}
-
- */
-import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormBuilder,
-  FormArray
-} from '../../../../../node_modules/@angular/forms';
-
-@Component({
-  selector: 'app-pooling-creator',
-  templateUrl: './pooling-creator.component.html',
-  styleUrls: ['./pooling-creator.component.scss']
-})
-export class PoolingCreatorComponent implements OnInit {
-  invoiceForm: FormGroup;
-  default = 'single-selection-grid';
-  selects: Select[] = [
-    {
-      control: [
-        { value: 'short-answer', viewValue: 'Krótka odpowiedź' },
-        { value: 'long-answer', viewValue: 'Długa odpowiedź' }
-      ]
-    },
-    {
-      control: [
-        { value: 'single-choice', viewValue: 'Jednokrotny wybór' },
-        { value: 'multiple-choice', viewValue: 'Wielokrotny wybór' }
-      ]
-    },
-    {
-      control: [
-        { value: 'dropdown-menu', viewValue: 'Menu rozwijane' },
-        { value: 'linear-scale', viewValue: 'Skala liniowa' }
-      ]
-    },
-    {
-      control: [
-        {
-          value: 'single-selection-grid',
-          viewValue: 'Siatka jednokrotnego wyboru'
-        },
-        {
-          value: 'multiple-selection-grid',
-          viewValue: 'Siatka wielokrotnego wyboru'
-        }
-      ]
-    }
-  ];
-
-  minValue: Value[] = [{ value: 0 }, { value: 1 }];
-  maxValue: Value[] = [
-    { value: 2 },
-    { value: 3 },
-    { value: 4 },
-    { value: 5 },
-    { value: 6 },
-    { value: 7 },
-    { value: 8 },
-    { value: 9 },
-    { value: 10 }
-  ];
-  info = {
-    add: 'Dodaj pytanie',
-    copy: 'Duplikuj',
-    delete: 'Usuń pytanie'
-  };
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.invoiceForm = this.fb.group({
-      Form_Title: ['Formularz bez nazwy'],
-      QuestionData: this.fb.array([this.addRows()])
+  changeControl(FieldData, select, i) {
+    // console.log(FieldData);
+    // tslint:disable-next-line:prefer-const
+    // let formGroup;
+    // const stringArr = Object.keys(FieldData.controls[i].controls);
+    // console.log(stringArr);
+    // console.log(FieldData.controls[i].controls.field.controls);
+    // stringArr.forEach(string => {
+    // console.log(FieldData.controls[i].controls[string].controls);
+    //   formGroup.push(FieldData.controls[i].controls[string].controls);
+    // });
+    FieldData.removeAt(0);
+    this.addGroup(FieldData, select);
+    // switch (this.lastSelect) {
+    //   case 'single-choice':
+    //   case 'multiple-choice':
+    //     switch (select) {
+    //       case 'single-grid':
+    //       case 'multiple-grid':
+    //       case 'dropdown-menu':
+    //         // send formgroup
+    //         console.log('send');
+    //         FieldData.removeAt(0);
+    //         // this.addExampleArr(FieldData);
+    //         this.addArray(FieldData, select);
+    //         break;
+    //       default:
+    //         FieldData.removeAt(0);
+    //         this.addGroup(FieldData, select);
+    //         break;
+    //     }
+    //     break;
+    //   case 'single-grid':
+    //   case 'multiple-grid':
+    //     // //
+    //     switch (select) {
+    //       case 'single-choice':
+    //       case 'multiple-choice':
+    //       case 'dropdown-menu':
+    //         // send formgroup
+    //         console.log('send');
+    //         break;
+    //       default:
+    //         FieldData.removeAt(0);
+    //         this.addGroup(FieldData, select);
+    //         break;
+    //     }
+    //     break;
+    //   case 'dropdown-menu':
+    //     switch (select) {
+    //       case 'single-choice':
+    //       case 'multiple-choice':
+    //       case 'dropdown-menu':
+    //       case 'single-grid':
+    //       case 'multiple-grid':
+    //         // send formgroup
+    //         console.log('send');
+    //         break;
+    //       default:
+    //         FieldData.removeAt(0);
+    //         this.addGroup(FieldData, select);
+    //         break;
+    //     }
+    //     break;
+    //   default:
+    //     FieldData.removeAt(0);
+    //     this.addGroup(FieldData, select);
+    //     break;
+    // }
+    // if (this.lastSelect) {
+    //   this.lastSelect = select;
+    // }
+    /*     // this.time1 = Date.now();
+    // let group;
+    const stringArr = Object.keys(FieldData.controls[i].controls);
+    console.log(stringArr);
+    // console.log(FieldData.controls[i].controls.field.controls);
+    stringArr.forEach(string => {
+      console.log(FieldData.controls[i].controls[string].controls);
+      // group.push(FieldData.controls[i].controls[string].controls);
     });
+    // switch (stringArr[0]) {
+    //   case 'field':
+    //   break;
+    // }
+    FieldData.removeAt(0);
+    console.log(select);
+    this.addGroup(FieldData, select);
+    // if(stringArr[0] === 'field'){
+    //   if(select)
+    // } */
   }
-
-  addRows() {
-    const group = this.fb.group({
-      question: [''],
-      select: [this.default],
-      FieldData: this.fb.array([])
-    });
-    this.addGroup(group.controls.FieldData, group.controls.select.value);
-    return group;
-  }
-
-  addQuestion() {
-    const control: FormArray = this.invoiceForm.get(
-      `QuestionData`
-    ) as FormArray;
-    control.push(this.addRows());
-  }
-  removeQuestion(index) {
-    const questionArr = this.invoiceForm.controls.QuestionData as FormArray;
-    console.log(questionArr.controls.length);
-    if (questionArr.controls.length > 1) {
-      questionArr.removeAt(index);
-    } else {
-      questionArr.removeAt(0);
-      questionArr.push(this.addRows());
-    }
-  }
-
-  addGroup(FieldData, select: string) {
-    let group;
-    switch (select) {
-      case 'short-answer':
-        group = this.addInput();
-        FieldData.push(group);
-        break;
-      case 'long-answer':
-        group = this.addInput();
-        FieldData.push(group);
-        break;
-      case 'dropdown-menu':
-        group = this.addMenu();
-        FieldData.push(group);
-        this.addField(select, group.controls.dropdown_menu);
-        break;
-      case 'linear-scale':
-        group = this.addLinearScale();
-        FieldData.push(group);
-        this.addField(select, group.controls.linear_scale);
-        break;
-      case 'single-choice':
-        group = this.addSingleChoice();
-        FieldData.push(group);
-        this.addField(select, group.controls.single_choice);
-        break;
-      case 'multiple-choice':
-        group = this.addMultipleChoice();
-        FieldData.push(group);
-        this.addField(select, group.controls.multiple_choice);
-        break;
-      case 'single-selection-grid':
-        group = this.addSingleSelectionGrid();
-        FieldData.push(group);
-        this.addField(select, group.controls.columns, group.controls.rows);
-        break;
-      case 'multiple-selection-grid':
-        group = this.addSingleSelectionGrid();
-        FieldData.push(group);
-        this.addField(select, group.controls.columns, group.controls.rows);
-        break;
-    }
-  }
-  addField(select: string, choiceData, choiceData2?) {
-    switch (select) {
-      case 'dropdown-menu':
-        this.addSelectOption(choiceData);
-        break;
-      case 'linear-scale':
-        this.addLinearScaleField(choiceData);
-        break;
-      case 'single-choice':
-        this.addSingleMultipleField(choiceData);
-        break;
-      case 'multiple-choice':
-        this.addSingleMultipleField(choiceData);
-        break;
-      case 'single-selection-grid':
-        this.addSingleSelectionField(choiceData);
-        this.addSingleSelectionInput(choiceData2);
-        break;
-      case 'multiple-selection-grid':
-        this.addSingleSelectionField(choiceData);
-        this.addSingleSelectionInput(choiceData2);
-        break;
-    }
-  }
-
-  addInput() {
-    const group = this.fb.group({
-      input: [{ value: '', disabled: true }]
-    });
-    return group;
-  }
-  addLinearScale() {
-    const group = this.fb.group({
-      linear_scale: this.fb.array([])
-    });
-    return group;
-  }
-  addSingleChoice() {
-    const group = this.fb.group({
-      single_choice: this.fb.array([])
-    });
-    return group;
-  }
-  addMultipleChoice() {
-    const group = this.fb.group({
-      multiple_choice: this.fb.array([])
-    });
-    return group;
-  }
-  addMenu() {
-    const group = this.fb.group({
-      dropdown_menu: this.fb.array([])
-    });
-    return group;
-  }
-  addSingleSelectionGrid() {
-    const group = this.fb.group({
-      columns: this.fb.array([]),
-      rows: this.fb.array([])
-    });
-    return group;
-  }
-  addLinearScaleField(choiceArr) {
-    const group = this.fb.group({
-      minValue: 1,
-      maxValue: 6,
-      minLabel: [''],
-      maxLabel: ['']
-    });
-    choiceArr.push(group);
-  }
-  addSelectOption(choiceArr) {
-    const length = choiceArr.controls.length;
-    const group = this.fb.group({
-      input: 'opcja ' + length
-    });
-    choiceArr.push(group);
-  }
-  addSingleMultipleField(choiceArr) {
-    const length = choiceArr.controls.length;
-    const group = this.fb.group({
-      value: [{ value: false, disabled: true }],
-      viewValue: 'opcja ' + length
-    });
-    choiceArr.push(group);
-  }
-  addSingleSelectionField(selectionArr) {
-    const length = selectionArr.controls.length;
-    const group = this.fb.group({
-      // value: [{ value: false, disabled: true }],
-      value: false,
-      viewValue: 'kolumna ' + length
-    });
-    selectionArr.push(group);
-  }
-  addSingleSelectionInput(selectionArr) {
-    const length = selectionArr.controls.length;
-    const group = this.fb.group({
-      input: 'wiersz ' + length
-    });
-    selectionArr.push(group);
-  }
-
-  removeField(index, SingleChoice) {
-    SingleChoice.removeAt(index);
-  }
-
-  updateSelection(SingleChoice, choice, e) {
-    if (e.source) {
-      console.log(e.source.name, e);
-    }
-    SingleChoice.controls.forEach(el => {
-      el.controls.value.setValue(false);
-    });
-    choice.controls.value.setValue(true);
-    e.source.checked = true;
-  }
-
-  changeControl(controls, select) {
-    controls.removeAt(0);
-    this.addGroup(controls, select);
-  }
-
-  changeSingleValue(single, e) {
-    // single.viewValue = e.target.value;
-  }
+  //   addExampleArr(FieldData) {
+  //     const group = this.fb.group({
+  //       field: this.fb.array([])
+  //     });
+  //     FieldData.push(group);
+  //   }
 }
 
 export class Control {
