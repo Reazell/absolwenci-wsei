eba6d119c6038be0822b9653a5de9a84d27c69eb
Filip Lipowiecki <lipowiecki.f@gmail.com>
2018-09-15 12:29:27 +0200

    .

--- CareerMonitoring.Core/Domains/Surveys/Survey.cs
+++ CareerMonitoring.Core/Domains/Surveys/Survey.cs
@@ -7,12 +7,6 @@ namespace CareerMonitoring.Core.Domains.Surveys {
         public string Title { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public bool Answered { get; private set; }
-        // public ICollection<LinearScale> LinearScales { get; private set; } = new List<LinearScale>();
-        // public ICollection<SingleChoice> SingleChoices { get; private set; } = new List<SingleChoice>();
-        // public ICollection<MultipleChoice> MultipleChoices { get; private set; } = new List<MultipleChoice>();
-        // public ICollection<OpenQuestion> OpenQuestions { get; private set; } = new List<OpenQuestion>();
-        // public ICollection<SingleGrid> SingleGrids { get; private set; } = new List<SingleGrid>();
-        // public ICollection<MultipleGrid> MultipleGrids { get; private set; } = new List<MultipleGrid>();
         public ICollection<Question> Questions { get; private set; }
 
         private Survey () { }
@@ -26,36 +20,6 @@ namespace CareerMonitoring.Core.Domains.Surveys {
         {
             Questions = questions;
         }
-
-        // public void AddLinearScale (LinearScale linearScale)
-        // {
-        //     LinearScales.Add(linearScale);
-        // }
-
-        // public void AddSingleChoice (SingleChoice singleChoice)
-        // {
-        //     SingleChoices.Add(singleChoice);
-        // }
-
-        // public void AddMultipleChoice (MultipleChoice multipleChoice)
-        // {
-        //     MultipleChoices.Add(multipleChoice);
-        // }
-
-        // public void AddOpenQuestion (OpenQuestion openQuestion)
-        // {
-        //     OpenQuestions.Add(openQuestion);
-        // }
-
-        // public void AddSingleGrid (SingleGrid singleGrid)
-        // {
-        //     SingleGrids.Add(singleGrid);
-        // }
-
-        // public void AddMultipleGrid (MultipleGrid multipleGrid)
-        // {
-        //     MultipleGrids.Add(multipleGrid);
-        // }
 
         public void MarkAsAnswered ()
         {
