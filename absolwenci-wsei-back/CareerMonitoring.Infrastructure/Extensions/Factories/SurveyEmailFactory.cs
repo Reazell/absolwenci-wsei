a79f88cf6acd45247ace1f0bdc2fc6f9225680d7
Ami <amadoucoulibaly343@onet.pl>
2018-10-10 13:16:10 +0200

    SurveyEmailFactory html body changes

--- absolwenci-wsei-back/CareerMonitoring.Infrastructure/Extensions/Factories/SurveyEmailFactory.cs
+++ absolwenci-wsei-back/CareerMonitoring.Infrastructure/Extensions/Factories/SurveyEmailFactory.cs
@@ -15,23 +15,27 @@ namespace CareerMonitoring.Infrastructure.Extensions.Factories {
         private readonly ISurveyUserIdentifierService _surveyUserIdentifierService;
         private readonly IEncryptorFactory _encryptorFactory;
         private readonly IUnregisteredUserRepository _unregisteredUserRepository;
+        private readonly ISurveyRepository _surveyRepository;
 
         public SurveyEmailFactory (IEmailConfiguration emailConfiguration,
             IEmailFactory emailFactory,
             IAccountRepository accountRepository,
             ISurveyUserIdentifierService surveyUserIdentifierService,
             IEncryptorFactory encryptorFactory,
-            IUnregisteredUserRepository unregisteredUserRepository) {
+            IUnregisteredUserRepository unregisteredUserRepository,
+            ISurveyRepository surveyRepository) {
             _surveyUserIdentifierService = surveyUserIdentifierService;
             _encryptorFactory = encryptorFactory;
             _emailConfiguration = emailConfiguration;
             _emailFactory = emailFactory;
             _accountRepository = accountRepository;
             _unregisteredUserRepository = unregisteredUserRepository;
+            _surveyRepository = surveyRepository;
         }
 
         public async Task SendSurveyEmailAsync (int surveyId) {
             var accounts = await _accountRepository.GetAllAsync ();
+            var survey = await _surveyRepository.GetByIdAsync(surveyId,false);
             foreach (var account in accounts) {
                 if (account.Role != "careerOffice") {
                     var message = new MimeMessage ();
@@ -39,7 +43,43 @@ namespace CareerMonitoring.Infrastructure.Extensions.Factories {
                     message.To.Add (new MailboxAddress (account.Name, account.Email));
                     message.Subject = "Monitorowanie karier - ankieta";
                     message.Body = new TextPart ("html") {
-                        Text = $"Witaj! Biuro karier WSEI zaprasza do wypełnienia krótkiej ankiety. Aby przejść do ankiety klinkij w ten <a href=\"http://localhost:4200/api/survey/surveys/{surveyId}/{_encryptorFactory.EncryptStringValue(account.Email)}\">link</a> ."
+                        Text = "<div style=\"padding: 10px;\"> " +
+                            "<table width=\"100%\">" +
+                            "<tbody>" +
+                            "<tr id=\"header\">" +
+                            "<td height=\"40\" style=\"background-color: #8bc34a; padding: 25px; text-align: center; color: white;\">" +
+                            $"{survey.Title}" +
+                            "</td>" +
+                            "</tr>" +
+                            "<tr>" +
+                            "<td height=\"30\"></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                            "<td style=\"text-align: center;\"><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+                            "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
+                            "ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                            "<td height=\"30\"></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                            "<tr>" +
+                            "<td style=\"text-align: center; font-weight: 600;\"><span>Zapraszamy do wypełnienia ankiety</span></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                            "<td height=\"30\"></td>" +
+                            "</tr>" +
+                            "<tr style=\"justify-content: center; display: flex;\">" +
+                            "<td style=\"display: flex; justify-content: center;\">" +
+                            "<div style=\"border-radius: 3px; background-color: #8bc34a; padding: 10px; text-align: center; max-width: 150px;\">" +
+                            $"<a style=\"text-decoration: none; color: white; outline: none;\" href=\"http://localhost:4200/api/survey/surveys/{surveyId}/{_encryptorFactory.EncryptStringValue(account.Email)}\">Przejdź do ankiety </a>" +
+                            "</div>" +
+                            "</td>" +
+                            "</tr>" +
+                            "<tr></tr>" +
+                            "</tbody>" +
+                            "</table>" +
+                            "</div>"
                     };
                     await _emailFactory.SendEmailAsync (message);
                     await _surveyUserIdentifierService.CreateAsync (account.Email, surveyId);
@@ -49,6 +89,7 @@ namespace CareerMonitoring.Infrastructure.Extensions.Factories {
 
         public async Task SendSurveyEmailToUnregisteredUsersAsync (int surveyId) {
             var unregisteredUsers = await _unregisteredUserRepository.GetAllAsync ();
+            var survey = await _surveyRepository.GetByIdAsync(surveyId,false);
             foreach (var unregisteredUser in unregisteredUsers) {
                 if (unregisteredUser.Role != "careerOffice") {
                     var message = new MimeMessage ();
@@ -56,7 +97,44 @@ namespace CareerMonitoring.Infrastructure.Extensions.Factories {
                     message.To.Add (new MailboxAddress (unregisteredUser.Name, unregisteredUser.Email));
                     message.Subject = "Monitorowanie karier - ankieta";
                     message.Body = new TextPart ("html") {
-                        Text = $"Witaj! Biuro karier WSEI zaprasza do wypełnienia krótkiej ankiety. Aby przejść do ankiety klinkij w ten <a href=\"http://localhost:4200/api/survey/surveys/{surveyId}\">link</a> ."
+                        Text = "<div style=\"padding: 10px;\"> " +
+                            "<table width=\"100%\">" +
+                            "<tbody>" +
+                            "<tr id=\"header\">" +
+                            "<td height=\"40\" style=\"background-color: #8bc34a; padding: 25px; text-align: center; color: white;\">" +
+                            $"{survey.Title}" +
+                            "</td>" +
+                            "</tr>" +
+                            "<tr>" +
+                            "<td height=\"30\"></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                            "<td style=\"text-align: center;\"><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod" +
+                            "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation" +
+                            "ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                            "<td height=\"30\"></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                            "<tr>" +
+                            "<td style=\"text-align: center; font-weight: 600;\"><span>Zapraszamy do wypełnienia ankiety</span></td>" +
+                            "</tr>" +
+                            "<tr>" +
+                            "<td height=\"30\"></td>" +
+                            "</tr>" +
+                            "<tr style=\"justify-content: center; display: flex;\">" +
+                            "<td style=\"display: flex; justify-content: center;\">" +
+                            "<div style=\"border-radius: 3px; background-color: #8bc34a; padding: 10px; text-align: center; max-width: 150px;\">" +
+                            $"<a style=\"text-decoration: none; color: white; outline: none;\" href=\"http://localhost:4200/api/survey/surveys/{surveyId}/{_encryptorFactory.EncryptStringValue(unregisteredUser.Email)}\">Przejdź do ankiety </a>" +
+                            "</div>" +
+                            "</div>" +
+                            "</td>" +
+                            "</tr>" +
+                            "<tr></tr>" +
+                            "</tbody>" +
+                            "</table>" +
+                            "</div>"
                     };
                     await _emailFactory.SendEmailAsync (message);
                 }
