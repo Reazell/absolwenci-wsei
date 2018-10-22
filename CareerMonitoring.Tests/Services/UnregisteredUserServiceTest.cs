626f9c3e34a26acf096bb168b4c6c7129205439a
Filip Lipowiecki <lipowiecki.f@gmail.com>
2018-10-22 09:52:32 +0200

    TestsProject

--- CareerMonitoring.Tests/Services/UnregisteredUserServiceTest.cs
+++ CareerMonitoring.Tests/Services/UnregisteredUserServiceTest.cs
@@ -22,43 +22,8 @@ namespace CareerMonitoring.Tests.Services
                 "john@gmail.com");
             //Assert
             unregisteredUserRepositoryMock.Verify(x=>x.AddAsync(It.IsAny<UnregisteredUser>()),Times.Once());
-
         }
-//        [Fact]
-//             public async Task Get_all_async_should_invoke_get_all_async_on_user_repository()
-//             {
-//                 //Arrange
-//                 DateTime date = Convert.ToDateTime("2018-01-01 00:00:00");
-//                 var unregisteredUser = new UnregisteredUser("john", "johnson", "Informatyka i ekonometria", date, "dzienne",
-//                     "john@gmail.com");
-//                 var unregisteredUser2 = new UnregisteredUser("john", "johnson", "Informatyka i ekonometria", date, "dzienne",
-//                     "john@gmail.com");
-//                 List<UnregisteredUser> unregisteredUsersList = new List<UnregisteredUser>();
-//                 unregisteredUsersList.Add(unregisteredUser);
-//                 unregisteredUsersList.Add((unregisteredUser2));
-//                 var unregisteredUserRepositoryMock = new Mock<IUnregisteredUserRepository>().Setup(x=>x.GetAllAsync()).Returns();
-//                 var unregisteredUserService = new UnregisteredUserService(unregisteredUserRepositoryMock);
-//                 //Act
-//                 var unregisteredUsers = await unregisteredUserService.GetAllAsync();
-//                 //Assert
-//                 unregisteredUserRepositoryMock.Verify(x=>x.GetAllAsync(It.IsInRange(),Times.Once());
-//             }
-//             [Fact]
-//             public async Task Delete_async_should_invoke_Delete_async_on_user_repository()
-//             {
-//                 //Arrange
-//                 DateTime date = Convert.ToDateTime("2018-01-01 00:00:00");
-//                 var unregisteredUser = new UnregisteredUser("john", "johnson", "Informatyka i ekonometria", date, "dzienne",
-//                     "john@gmail.com");
-//                 var unregisteredUserRepositoryMock = new Mock<IUnregisteredUserRepository>().Setup(x=>x.DeleteAsync(unregisteredUser));
-//                 var unregisteredUserService = new UnregisteredUserService(unregisteredUserRepositoryMock);
-//                 //Act
-//                 await unregisteredUserService.CreateAsync("john", "johnson", "Informatyka i ekonometria", "2018-01-01 00:00:00", "dzienne",
-//                     "john@gmail.com");
-//                 //Assert
-//                 unregisteredUserRepositoryMock.Verify(x=>x.AddAsync(It.IsAny<UnregisteredUser>()),Times.Once());
-//             }
-         }
+    }
 }
 
 
