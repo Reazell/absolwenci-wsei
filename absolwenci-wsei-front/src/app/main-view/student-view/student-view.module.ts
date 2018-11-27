7f882a8687492312e3291c8740e9e989d65fea01
RocketDuke <krzysmyjak@gmail.com>
2018-11-27 13:57:40 +0100

    init

--- absolwenci-wsei-front/src/app/main-view/student-view/student-view.module.ts
+++ absolwenci-wsei-front/src/app/main-view/student-view/student-view.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentViewComponent } from './student-view.component';
 import { StudentViewRoutingModule } from './student-view.routing';
+import { StudentProfileComponent } from './student-profile/student-profile.component';
 
 @NgModule({
   imports: [
     CommonModule,
     StudentViewRoutingModule
   ],
-  declarations: [StudentViewComponent]
+  declarations: [StudentViewComponent, StudentProfileComponent]
 })
 export class StudentViewModule { }
